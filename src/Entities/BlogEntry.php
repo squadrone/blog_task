0dd7a2fdf72a6fa5a25d49c0f0d5a9b38317e0f7
Merter Koyuncu <merterkoyuncu@gmail.com>
2023-08-14 15:19:49 +0300

    Entities have been completed.

--- src/Entities/BlogEntry.php
+++ src/Entities/BlogEntry.php
@@ -4,5 +4,70 @@ namespace Project\Entities;
 
 class BlogEntry
 {
+    private int $id;
+    private User $user;
+    private string $title;
+    private string $content;
+    private string $imageUrl;
+    private string $createdAt;
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getImageUrl(): string
+    {
+        return $this->imageUrl;
+    }
+
+    public function setImageUrl(string $imageUrl): void
+    {
+        $this->imageUrl = $imageUrl;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(string $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
 }
---
+++ src/Entities/Comment.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Project\Entities;
+
+class Comment
+{
+    private int $id;
+    private BlogEntry $blogEntry;
+    private string $name;
+    private string $email;
+    private string $url;
+    private string $message;
+    private string $createdAt;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getBlogEntry(): BlogEntry
+    {
+        return $this->blogEntry;
+    }
+
+    public function setBlogEntry(BlogEntry $blogEntry): void
+    {
+        $this->blogEntry = $blogEntry;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(string $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+}
